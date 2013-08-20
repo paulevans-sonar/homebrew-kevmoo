eb924a58c6e68973e664563bd28725c1dae0fbee
Kevin Moore <kevin@thinkpixellab.com>
2013-08-20 09:47:21 -0700

    trunk 26297

--- dart-editor.rb
+++ dart-editor.rb
@@ -38,9 +38,9 @@ class DartEditor < Formula
   sha1 'ca133b6844b905bbb439a20ca45e026cd3f666f3'
 
   devel do
-    url 'https://gsdview.appspot.com/dart-editor-archive-trunk/26023/darteditor-macos-64.zip'
-    version '26023'
-    sha1 'd3c61d918c0a5c661d72fb2fa69251f36cfb3cae'
+    url 'https://gsdview.appspot.com/dart-editor-archive-trunk/26297/darteditor-macos-64.zip'
+    version '26297'
+    sha1 'bccbecbf8b1223ad47e312076845537bbc811712'
   end
 
   depends_on Requires64Bit
