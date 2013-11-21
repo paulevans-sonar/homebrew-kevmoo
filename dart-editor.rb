88a3679650b3fcb68f0d52407bd7291c3349b94e
Kevin Moore <kevin@thinkpixellab.com>
2013-11-21 10:22:16 -0800

    dart-editor: devel now points to 30338

--- dart-editor.rb
+++ dart-editor.rb
@@ -32,19 +32,36 @@ class RequiresNoDart < Requirement
 end
 
 class DartEditor < Formula
-  VERSION = '30188'
-  BASE_URL = "https://gsdview.appspot.com/dart-archive/channels/stable/release/#{VERSION}/"
+  ROOT_URL = "https://gsdview.appspot.com/dart-archive/channels"
+  release_version = '30188'
+
+  base_url = "#{ROOT_URL}/stable/release/#{release_version}"
 
   homepage 'http://www.dartlang.org/'
-  url "#{BASE_URL}editor/darteditor-macos-x64.zip"
-  version VERSION
+  url "#{base_url}/editor/darteditor-macos-x64.zip"
+  version release_version
   sha1 'e6eb7099ea883d2d6790c006dcb575245cf89031'
 
   option 'with-content-shell', 'Download and install content_shell -- headless Chromium for testing'
 
+  devel do
+    dev_release_version = '30338'
+    dev_base_url = "#{ROOT_URL}/dev/release/#{dev_release_version}"
+
+    url "#{dev_base_url}/editor/darteditor-macos-x64.zip"
+    version dev_release_version
+    sha1 'f41242671f4e11ef735d7c6d40c33507a57c6c7a'
+
+    resource 'content_shell' do
+      url "#{dev_base_url}/dartium/content_shell-macos-ia32-release.zip"
+      version dev_release_version
+      sha1 '681fb723f4d4b2eeec9ecc0563a5d530ad25f3fa'
+    end
+  end
+
   resource 'content_shell' do
-    url "#{BASE_URL}dartium/content_shell-macos-ia32-release.zip"
-    version VERSION
+    url "#{base_url}/dartium/content_shell-macos-ia32-release.zip"
+    version release_version
     sha1 '4d7514c7d83ccb4a442afb4a71bde7d1c020d688'
   end
 
