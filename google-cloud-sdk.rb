cee4c3e616f017622259eb05a271b1178bbf8dbc
Kevin Moore <github@j832.com>
2013-12-31 10:04:33 -0800

    starting on google cloud sdk

---
+++ google-cloud-sdk.rb
@@ -0,0 +1,6 @@
+require 'formula'
+
+class GoogleCloudSDK < Formula
+  url 'https://dl.google.com/dl/cloudsdk/release/google-cloud-sdk.tar.gz'
+  sha1 '3a09d6d5d4e31c702f17e664a527b4c2f6e84faf'
+end
